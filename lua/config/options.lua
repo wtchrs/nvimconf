cc057843cb1a57dcf289552d084b2ae1e7f8907a
wtchrs <wtchr_@hotmail.com>
2024-12-03 02:19:33 +0900

    Update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set Powershell as default terminal if OS is Windows
+WINDOWS = "Windows_NT"
+if string.sub(vim.loop.os_uname().sysname, 1, string.len(WINDOWS)) == WINDOWS then
+  vim.o.shell = "pwsh.exe"
+end
+
+-- Set markdown rendering disable
+vim.opt.conceallevel = 0
